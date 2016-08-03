b6c9df86aacea945c88f1e93f2d583a799a211a8
Vivek Bharatha <vivek.bharatha@gmail.com>
2016-08-03 06:35:15 +0000

    Added date parsing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = new express();
+var strftime = require('strftime');
 app.get('/:input?', function (req, res, next) {
    var input = req.params.input;
    var resJson = {
@@ -7,6 +8,14 @@ app.get('/:input?', function (req, res, next) {
        natural: null
    };
    if (input === undefined) return res.json(resJson);
+   
+   var date = new Date(input);
+   console.log('input', input, 'date', date, '\n-------------');
+   
+   resJson.unix = Math.floor(date.getTime()/1000);
+   resJson.natural = strftime('%B %e, %Y', date);
+   
+   return res.json(resJson);
    
 });
 
